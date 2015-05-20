b6ef51dfa188e50e53ab3ac3fea562b48618c5ca
Josh Padnick <josh.padnick@gmail.com>
2015-05-19 22:12:20 -0700

    Updated docs.

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author Josh Padnick
+ */
+
+public class Main {
+
+    /**
+     * This main method is meant to be executed from Maven.  See README.md for more info.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+    }
+
+}
